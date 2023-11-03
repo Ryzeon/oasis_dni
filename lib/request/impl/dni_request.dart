0f929fd74ce17b577e48e9a4a7ec6dc66f6b1f87
Ryzeon <alex.ramon2004@gmail.com>
2023-11-02 22:37:59 -0500

    added dni request

---
+++ lib/request/impl/dni_request.dart
@@ -0,0 +1,14 @@
+import 'package:oasis_dni/model/dni.dart';
+import 'package:oasis_dni/request/request.dart';
+
+class DNIRequest extends IRequest {
+  @override
+  String getPath() {
+    return "/customer/dni/";
+  }
+  
+  @override
+  DNI getModel(Map<String, dynamic> json) {
+    return DNI.fromJson(json);
+  }
+}
--- lib/request/request_container.dart
+++ lib/request/request_container.dart
@@ -1,3 +1,4 @@
+import 'package:oasis_dni/request/impl/dni_request.dart';
 import 'package:oasis_dni/request/request.dart';
 
 class RequestContainer {
@@ -7,7 +8,9 @@ class RequestContainer {
   RequestContainer._internal();
 
   // Create variables to store data
-  final Map<String, IRequest> _requests = Map.from({});
+  final Map<String, IRequest> _requests = Map.from({
+    "dni": DNIRequest(),
+  });
 
   IRequest<T> getRequest<T>(String key) {
     return _requests[key] as IRequest<T>;
