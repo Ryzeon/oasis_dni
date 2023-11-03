3e408aee8b2cd70911ce11ddb858c2da9d3f0958
Ryzeon <alex.ramon2004@gmail.com>
2023-11-03 00:16:43 -0500

    + added spin animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'package:oasis_dni/env/environment.dart';
 import 'package:oasis_dni/lang.dart';
 import 'package:oasis_dni/screens/login_screen.dart';
 
 Future<void> main() async {
-  await dotenv.load(
-    fileName: Envionment.fileName
-  );
+  await dotenv.load(fileName: Envionment.fileName);
 
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
@@ -18,18 +18,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        secondaryHeaderColor: Lang.primaryColor.getColor(),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return GlobalLoaderOverlay(
+      useDefaultLoading: false,
+      overlayWidget: const Center(
+        child: SpinKitCubeGrid(
+          color: Colors.green,
+          size: 50.0,
+        ),
       ),
-      title: 'Hotel DNI',
-      routes: {
-        'login': (_) => const LoginScreen()
-      },
-      initialRoute: "login",
+      overlayColor: Colors.green,
+      overlayWholeScreen: false,
+      overlayOpacity: 0.8,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+          secondaryHeaderColor: Lang.primaryColor.getColor(),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        title: 'Hotel DNI',
+        routes: {'login': (_) => const LoginScreen()},
+        initialRoute: "login",
+      ),
     );
   }
 }
